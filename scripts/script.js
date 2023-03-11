f29c550c862ca70c316c77883401d1ecaebbe6b8
lizeth montoya <lizethmontoyagarcia@gmail.com>
2023-03-11 14:08:00 -0500

    VideoTube

---
+++ scripts/script.js
@@ -0,0 +1,9 @@
+// llamamos la carpeta de video.js
+import { videoTube } from "./video.js";
+console.log(videoTube);
+
+//capturamos el contenedor 
+const containerVideo = document.querySelector(".main_video");
+console.log(containerVideo);
+
+
---
+++ scripts/video.js
@@ -0,0 +1,94 @@
+ export const videoTube = [
+    {
+      id: 1,
+      name: "El Chavo Animado (Capítulo 76) *La Casa del Árbol* (1/2) (NUEVO)",
+      video: "https://www.youtube.com/embed/9U4LhNCjVPU",
+      duration: "93 minutos",
+    },
+    {
+      id: 2,
+      name: "el juego  DEL CALAMAR en halloween   Desocupe masivo Squid Game",
+      video: "https://www.youtube.com/embed/OSASr0-FUQk",
+      duration: "93 minutos",
+    },
+    {
+      id: 3,
+      name: "Curso JavaScript: 46. Callbacks - #jonmircha",
+      video: "https://www.youtube.com/embed/TYG2_iUr8XY",
+      duration: "93 minutos",
+    },
+    {
+      id: 4,
+      name: "Homero roba con un Parquimetro Los simpsons capitulos completos en español latino",
+      video: "https://www.youtube.com/embed/FOFPh_dAg6g",
+      duration: "93 minutos",
+    },
+    {
+      id: 5,
+      name: "Cuidate del hombre Coop + Truco o Golosina 🐈 Kid Vs. Kat 🐈 Niño Vs. Gato - WildBrain",
+      video: "https://www.youtube.com/embed/YROXZU8oZU4",
+      duration: "93 minutos",
+    },
+    {
+      id: 6,
+      name: "Formulario animado con HTML, CSS y Javascript",
+      video: "https://www.youtube.com/embed/iN0xYdgRAzk",
+      duration: "93 minutos",
+    },
+    {
+      id: 7,
+      name: "GAP: GLUTEOS, ABDOMEN, Y PIERNAS + BRAZOS",
+      video: "https://www.youtube.com/embed/AUTqIj21X7g",
+      duration: "93 minutos",
+    },
+    {
+      id: 8,
+      name: "ACTIVA TU CEREBRO AL 100 por ciento con este test de VELOCIDAD MENTAL",
+      video: "https://www.youtube.com/embed/YYCQP5FKGsM",
+      duration: "93 minutos",
+    },
+    {
+      id: 9,
+      name: "Si tienes pasta en casa, ¡haz esta deliciosa receta que a todos les encantará!",
+      video: "https://www.youtube.com/embed/hV2x9wjJ3I8",
+      duration: "93 minutos",
+    },
+    {
+      id: 10,
+      name: "15 PLANTAS QUE PUEDES CULTIVAR FÁCILMENTE EN TU PROPIA COCINA",
+      video: "https://www.youtube.com/embed/S5a3cCXvmGc",
+      duration: "93 minutos",
+    },
+    {
+      id: 11,
+      name: "Cómo cuidar las plantas   De manera correcta Desocupe Masivo",
+      video: "https://www.youtube.com/embed/l08CTff43k8",
+      duration: "93 minutos",
+    },
+    {
+      id: 12,
+      name: "Birria de Res FÁCIL | La Capital",
+      video: "https://www.youtube.com/embed/g2F5RO6vNSs",
+      duration: "93 minutos",
+    },
+    {
+      id: 13,
+      name: "EL CABO DE LA VELA 🪁 PSICODÉLICO VIAJE en MOTO por LA GUAJIRA | Episodio 125 Vuelta al Mundo en Moto",
+      video: "https://www.youtube.com/embed/Q5J6eImVS5g",
+      duration: "93 minutos",
+    },
+    {
+      id: 14,
+      name: "Arrays y Objetos en JavaScript en 15 Minutos",
+      video: "https://www.youtube.com/embed/5DaZXXbHI_U",
+      duration: "93 minutos",
+    },
+    {
+      id: 15,
+      name: "Curso Basico de CSS - 2. Selectores",
+      video: "https://www.youtube.com/embed/uqkj7HQ7ids",
+      duration: "93 minutos",
+    },
+  ];
+  
+  
