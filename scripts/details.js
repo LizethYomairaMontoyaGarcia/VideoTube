c851e42fd9de07a7957540c3a747caf4b11c4b69
lizeth montoya <lizethmontoyagarcia@gmail.com>
2023-03-21 00:18:44 -0500

    estilos de la ventana de reproduccion

--- scripts/details.js
+++ scripts/details.js
@@ -19,11 +19,9 @@ title.innerText = video.name;
 
 const iframe = document.getElementById("iframe");
 console.log(iframe)
-iframe.innerHTML =  `<article class="container__video" data-video="video"  name=${video.id} >
+iframe.innerHTML =  `<article class="container__video__Reproduce" data-video="video"  name=${video.id} >
 <iframe data-video="video" name=${video.id} src=${video.video}  alt=${video.name}>
 </article>`
-
-
 
 //------Escuchar el click del logo de la página para que redireccione a la página principal-----
 
@@ -31,4 +29,35 @@ const imagen = document.querySelector(".header__figure");
 
 imagen.addEventListener("click", () => {
   window.location.href = "../index.html";
+});
+
+
+
+
+
+
+//capturamos el contenedor para mostrar la lista de videos
+const containerVideo = document.querySelector(".main_video");
+console.log(containerVideo);
+
+const filterVideo = videoLista.filter((item)=> item.id !== idVideoTube);
+console.log(filterVideo)
+
+
+//permite pintar los videos dentro de un elemento contenedor
+const videoCharacters = (container, videoList) => {
+  //Vaciar el contenedor
+  container.innerHTML = "";
+  //Recorremos el array
+  videoList.forEach((video) => {
+    container.innerHTML += `
+    <article class="container__video" data-video="video"  name=${video.id} >
+    <img class="imgs__video" data-video="video" name=${video.id} src=${video.img}  alt=${video.name}>
+    <h4 class="video__name" data-video='video' name=${video.id}>${video.name}</h4>
+    </article>`;
+  });
+};
+
+document.addEventListener("DOMContentLoaded", () => {
+  videoCharacters(containerVideo, filterVideo);
 });
